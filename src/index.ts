882ccd072eaa4ffa89d2a70a6b2539961c47972a
Turbo <turbothinh@gmail.com>
2020-01-10 11:53:50 +0100

    Add example to use Path

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import fetch from 'node-fetch';
+import { add } from '@utils/math';
+import { randomize } from '@utils/randomize';
 
-fetch("https://pokeapi.co/api/v2/pokemon")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data.results);
-  })
-  .catch(function(reason) {
-    console.log(reason);
-  });
+const a = randomize();
+const b = randomize();
+const total = add(a, b);
+
+console.log(`${a} + ${b} = ${total}`);
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+	.then(response => {
+		return response.json();
+	})
+	.then(data => {
+		console.log(data.results);
+	})
+	.catch(function(reason) {
+		console.log(reason);
+	});
